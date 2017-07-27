00207c6be927994e56a5618617b758906e4415dc
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-07-27 22:44:06 +0200

    Added tests for movie comments section

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -7,4 +7,14 @@ describe "Movies requests", type: :request do
       expect(page).to have_selector("h1", text: "Movies")
     end
   end
+
+  describe 'show movie' do
+    let(:movie) { create :movie, :with_comments }
+
+    it 'displays right movie comments' do
+      visit "/movies/#{movie.id}"
+      expect(page).to have_selector('h3', text: 'Comments')
+      expect(page).to have_selector('div.well', count: 3)
+    end
+  end
 end
