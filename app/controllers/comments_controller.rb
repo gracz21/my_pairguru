df65004dc4f02803268eb9368bfa5be3da2a4714
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-09-03 14:03:04 +0200

    Refactored comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+  expose :comment, parent: :current_user
+
   def create
-    comment = current_user.comments.create(comment_params)
-    if comment.persisted?
+    if comment.save
       redirect_to movie_path(params[:movie_id]), notice: 'Comment created successfully'
     else
       redirect_to movie_path(params[:movie_id]), alert: 'New comment is empty or you have already commented this movie'
@@ -11,12 +12,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find_by_id(params[:id])
-    if comment.user_id == current_user.id
+    if comment.nil?
+      redirect_to movie_path(params[:movie_id]), alert: "You don't own this comment"
+    else
       comment.destroy
       redirect_to movie_path(params[:movie_id]), notice: 'Comment deleted successfully'
-    else
-      redirect_to movie_path(params[:movie_id]), alert: "You don't own this comment"
     end
   end
 
